40561126ab68b07bdf2030c2b395ac6d42982fe2
Shiva Prasad <shivapv@pm.me>
2020-12-16 13:02:38 +0530

    Create pdfless plugin files and main scripts
    Also add LICENSE, README, logo and favicon

---
+++ README.md
@@ -0,0 +1,47 @@
+![pdfless logo](plugin/images/logo-small.png)
+
+# pdfless
+
+*pdfless* is a customization plugin for [PDF.js][1] web viewer.
+It adds a switch for **Terminal mode** where you can change the colors and font
+of (non-scanned) PDF files. Read it as if it's on a terminal screen!
+
+## Installation
+
+1. [Download][2] the latest version (or clone this repo)
+2. `cd` into the root directory and run `./install.sh`
+3. Follow onscreen instructions
+
+## Notes
+
+Define your colors in `./plugin/colors.json` Default colors are taken from the
+[Smyck][3] colorscheme.
+
+Turning images off will improve performance and reduce overhead while switching
+between Terminal mode and normal view.
+
+*pdfless* does not attempt text-reflow (which is hopeless in PDFs). Resizing the
+text changes only the font-size without touching their position. This may result
+in disjoint / overlapping text; **this is not a bug**.
+
+## Why *pdfless*?
+
+I love reading on the terminal. The dark background, the colors, the monospaced
+font are all great for the eyes. But the stuff I have to read mostly are all in
+PDFs. PDFs are great for printing; but for reading, they suck. Bright white with
+dark text, fixed and rigid. Simply inverting the color is not a great solution;
+the font might still suck and images turn into their negative ghosts.
+
+PDF.js renders PDFs as HTML, enabling styling via CSS. *pdfless* adds a custom
+mode to the default viewer with controls to modify these styles on the go. Thus,
+it makes the PDF *less* rigid and suck *less* while reading. The name is also a
+shout-out to the Unix file-viewer utility `less`.
+
+There are PDF readers that does this in other ways (including Adobe reader), but
+I couldn't find one satisfactory. Many of them are also bloatwares (especially
+Adobe reader) and none of them goes as far as changing the font. PDF.js is small
+and runs decently in most modern browsers, requiring no additional software.
+
+[1]: https://mozilla.github.io/pdf.js/
+[2]: https://github.com/shivaprsd/pdfless/releases/latest
+[3]: https://github.com/hukl/Smyck-Color-Scheme 
---
+++ plugin/pdfless.js
@@ -0,0 +1,152 @@
+if (document.readyState === "interactive" || document.readyState === "complete") {
+  pdfLessInit();
+} else {
+  document.addEventListener("DOMContentLoaded", pdfLessInit, true);
+}
+
+async function pdfLessInit() {
+  linkCSS("../plugin/pdfless.css");
+  const colors = await fetch("../plugin/colors.json").then(resp => resp.json());
+  fetch("../plugin/pdfless.html")
+    .then(response => response.text()).then(addHTML)
+    .then(function() {
+      const config = getPdfLessConfig();
+      config.termColors = colors.termColors;
+      config.textColors = colors.textColors;
+      pdfLessLoad(config);
+    });
+
+  function addHTML(html) {
+    let docFrag = document.createRange().createContextualFragment(html);
+    let toolbar = document.getElementById("toolbarViewerRight");
+    toolbar.prepend(docFrag.getElementById("toolbarAddon").content);
+    let secToolbar = document.getElementById("secondaryToolbarButtonContainer");
+    secToolbar.prepend(docFrag.getElementById("secToolbarAddon").content);
+    document.head.appendChild(docFrag.getElementById("termModeTheme"));
+    document.head.appendChild(docFrag.getElementById("termModeFont"));
+  }
+
+  function linkCSS(href) {
+    let link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = href;
+    document.head.appendChild(link);
+  }
+}
+
+function getPdfLessConfig() {
+  return {
+    enableImg: true,
+    imageMode: false,
+    fontScaleFactor: 0.05,
+    termColors: {},
+    textColors: {},
+    docStyleElem: document.documentElement.style,
+    colorInputElem: document.getElementById("colorInput"),
+    termModeCSS: document.getElementById("termModeTheme"),
+    termModeFontCSS: document.getElementById("termModeFont")
+  };
+}
+
+function pdfLessLoad(config) {
+  config.termModeCSS.disabled = config.termModeFontCSS.disabled = true;
+  if (config.termColors.background) {
+    config.docStyleElem.setProperty("--termBG", config.termColors.background);
+  }
+  if (config.termColors.highlight) {
+    config.docStyleElem.setProperty("--termHL", config.termColors.highlight);
+  }
+  for (let col in config.textColors) {
+    config.colorInputElem.innerHTML +=
+      `<option value="${config.textColors[col]}">${col}</option>`;
+  }
+  if (parseFloat(pdfjsLib.version) < 2.6) {
+    config.docStyleElem.setProperty("--secToolbarWidth", "200px");
+    config.docStyleElem.setProperty("--secToolbarBtnPad", "24px");
+    config.docStyleElem.setProperty("--secToolbarIconLeft", "4px");
+  }
+
+  config.colorInputElem.onchange = function(e) {
+    config.docStyleElem.setProperty("--termColor", this.value);
+  }
+  document.getElementById("imageEnable").onchange = function(e) {
+    const termMode = !config.termModeCSS.disabled;
+    config.enableImg = this.checked;
+    if (termMode) {
+      if (config.enableImg) {
+        if (!config.imageMode) {
+          forceRedraw(true);
+          config.imageMode = true;
+        }
+        config.docStyleElem.setProperty("--canvasDisplay", "block");
+      } else {
+        config.docStyleElem.setProperty("--canvasDisplay", "none");
+      }
+    }
+  }
+  document.getElementById("fontInput").onchange = function(e) {
+    if (this.value) {
+      config.docStyleElem.setProperty("--termFont", this.value);
+      config.termModeFontCSS.disabled = false;
+    } else {
+      config.termModeFontCSS.disabled = true;
+    }
+  }
+  document.getElementById("fontReduce").onclick = function(e) {
+    termFontResize(1 - config.fontScaleFactor);
+  }
+  document.getElementById("fontEnlarge").onclick = function(e) {
+    termFontResize(1 + config.fontScaleFactor);
+  }
+  document.getElementById("termMode").onclick = toggleTermMode;
+  document.getElementById("secTermMode").onclick = toggleTermMode;
+
+  function toggleTermMode() {
+    const toTermMode = config.termModeCSS.disabled;
+    if (toTermMode) {
+      if (config.enableImg) {
+        forceRedraw(true);
+        config.imageMode = true;
+        config.docStyleElem.setProperty("--canvasDisplay", "block");
+      } else {
+        config.docStyleElem.setProperty("--canvasDisplay", "none");
+      }
+    } else {
+      if (config.imageMode) {
+        forceRedraw(false);
+        config.imageMode = false;
+      }
+      config.docStyleElem.setProperty("--canvasDisplay", "block");
+    }
+    config.termModeCSS.disabled = !toTermMode;
+  }
+
+  function termFontResize(scaleFactor) {
+    document.querySelectorAll(".textLayer > span").forEach(function(node) {
+      node.style.fontSize = parseFloat(node.style.fontSize) * scaleFactor + "px";
+    });
+  }
+
+  const ctxProto = CanvasRenderingContext2D.prototype;
+  const ctxProtoArr = [ctxProto.fillText, ctxProto.strokeText,
+                       ctxProto.fillRect, ctxProto.strokeRect];
+  function forceRedraw(imageMode) {
+    if (imageMode) {
+      ctxProto.fillText = ctxProto.strokeText = function() {};
+      ctxProto.fillRect = ctxProto.strokeRect = function() {};
+    } else {
+      [ctxProto.fillText, ctxProto.strokeText,
+       ctxProto.fillRect, ctxProto.strokeRect] = ctxProtoArr;
+    }
+    const pdfViewer = PDFViewerApplication.pdfViewer;
+    const i = pdfViewer.currentPageNumber;
+    for (let j = i - 9; j < i + 8; ++j) {
+      if (j >= 0 && j < pdfViewer.pagesCount) {
+        pdfViewer.getPageView(j).renderingState = 0;
+        pdfViewer.getPageView(j).reset();
+      }
+    }
+    pdfViewer.forceRendering();
+  }
+}
